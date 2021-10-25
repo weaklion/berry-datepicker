2579065c78511a8ecd40f62310366a14ceff9a52
weaklion <weaklion@naver.com>
2021-10-25 16:12:29 +0900

    yarn berry zero-install 적용

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return <div className="container">메인 화면입니다. </div>
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry : {
+    main : './src/index.js'
+  },
+  resolve : {
+    extensions : ['.js', '.jsx','.css']
+  },
+  devtool: 'eval-cheap-source-map',
+  devServer : {
+    hot : true,
+    overlay : true,
+    writeToDisk : true
+  },
+  output: {
+    filename : 'bundle.js',
+    path : path.resolve(__dirname, 'dist')
+  },
+  module : {
+    rules : [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: '/node_modules/',
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use : [
+          { loader : 'style-loader'},
+          { loader : 'css-loader'}
+        ]
+      },
+      {
+        test : /\.jfif$/,
+        loader : 'file-loader',
+        options : {
+          name : `[name].[ext]`
+        }
+      }
+    ]
+  },
+  plugins : [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+}
