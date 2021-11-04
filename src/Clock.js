1e38ae20c1032ad77ef0b10a8a8f46b47255de96
weaklion <weaklion@naver.com>
2021-11-04 12:42:51 +0900

    sass로 수정

--- src/Clock.js
+++ src/Clock.js
@@ -1,70 +1,6 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
 import { useInterval } from './useInterval';
-
-const ClockContainer = styled.div`
-  width: 30rem;
-  height: 30rem;
-  border: 1.5rem solid black;
-  border-radius : 50%;
-  margin:3rem auto;
-  position: relative;
-  padding: 2rem;
-`;
-
-const ClockFace = styled.div`
-  position : relative;
-  width: 100%;
-  height: 100%;
-`;
-
-const Circle = styled.div`
-  width : 2rem;
-  height: 2rem;
-  background-color: white;
-  position: absolute;
-  top : 50%;
-  left: 50%;
-  transform : translate(-50%, -50%);
-  border-radius : 100%;
-  z-index : 4;
-`;
-
-const Hand = styled.div`
-  width : 50%;
-  height: 6px;
-  background : black;
-  position : absolute;
-  top : 50%;
-  transition : 0.05s;
-  transform : rotate(90deg);
-  transform-origin: 100%;
-  transition-timing-function : cubic-bezier(0.1, 2.7, 0.58, 1);
-`;
-
-const HourHand = styled(Hand)`
-  z-index : 3;
-  width : 30%;
-  height : 8px;
-  left : 20%;
-  background : black;
-
-  transform : rotate(${(props) => props.hour}deg);
-`;
-const MinHand = styled(Hand)`
-  z-index : 2;
-  background : rgb(24,24,24);
-  transform : rotate(${(props) => props.min}deg);
-  height: 8px;
-`;
-
-const SecHand = styled(Hand)`
-  z-index : 1;
-  width : 40%;
-  left : 10%;
-  background : rgb(46,46,46);
-  transform : rotate(${(props) => props.sec}deg);
-`;
+import './Clock.scss';
 
 const Clock = () => {
 
@@ -83,19 +19,32 @@ const Clock = () => {
     setHour(hours);
   }, 1000);
 
+  const hourStyle = {
+    transform: `rotate(${hour}deg)`
+  };
+
+  const minStyle = {
+    transform: `rotate(${min}deg)`
+  };
+  
+  const secStyle = {
+    transform: `rotate(${second}deg)`
+  };
+  
+  
+
   return (
     <>
-      <ClockContainer>
-        <ClockFace>
-          <Circle/>
-          <HourHand hour={hour}/>
-          <MinHand min={min}/>
-          <SecHand sec={second}/>
-        </ClockFace>
-      </ClockContainer>
+      <div className="clock-container">
+        <div className="clock-face">
+          <div className="circle"/>
+          <div className="hand hour" style={hourStyle}/>
+          <div className="hand min" style={minStyle}/>
+          <div className="hand sec" style={secStyle}/>
+        </div>
+      </div>
     </>
   )
 }
-//sass로 변경
 
 export default Clock;
